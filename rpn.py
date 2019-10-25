2f3aa323d8c5d107f7b7f462e5290301e043b0c9
wujames <wujames@umich.edu>
2019-10-25 14:35:47 -0400

    finished rpn

--- rpn.py
+++ rpn.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-    pass
+    #pass
+    stack = list()
+    # range based for
+    for token in arg.split():
+        print(token)
+        if token == '+':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+
+        print(stack)
+
 
 def main():
     while True:
