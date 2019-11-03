fd400f8899e798860600f03521ccaa68a610b721
wujames <wujames@umich.edu>
2019-11-03 18:05:19 -0500

    updated travis

--- rpn.py
+++ rpn.py
@@ -16,6 +16,23 @@ def calculate(arg):
             arg1 = stack.pop()
             result = arg1 - arg2
             stack.append(result)
+        # ADDED HW8 -----
+        elif token == '*':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 * arg2
+            stack.append(result)
+        elif token == '/':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 / arg2
+            stack.append(result)
+        elif token == '^':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 * arg2
+            stack.append(result)
+        # ---------------
         else:
             stack.append(int(token))
 
--- test_rpn.py
+++ test_rpn.py
@@ -14,3 +14,7 @@ class TestBasics(unittest.TestCase):
     def test_badinput(self)
         with self.assertRaises(TypeError):
             rpn.calculate('1 2 3 +')
+    # ADDED HW8
+    def test_exp(self)
+        result = rpn.calculate('5 3 ^')
+        self.assertEqual(125, result)
